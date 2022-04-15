fbb033ffc7c26c2a0d65594c57def29508e8137a
Ol-create <oluola96@gmail.com>
2022-04-15 11:16:38 -0600

    add post request function

--- src/moudles/controller.js
+++ src/moudles/controller.js
@@ -2,3 +2,13 @@ const gameId = 'Zl4d7IVkemOTTVg2fUdzz'; //
 const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
 
 const gameScore = { user: '', score: 0 };
+
+// Post request
+const postData = async (data = {}) => {
+	const response = await fetch(baseUrl, {
+	  method: 'POST',
+	  headers: { 'Content-Type': 'application/json' },
+	  body: JSON.stringify(data),
+	});
+	return response.json();
+  };
