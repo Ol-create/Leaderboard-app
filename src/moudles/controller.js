66e1c33e58284332a0f1ff00227734b0fdbd8f7f
Ol-create <oluola96@gmail.com>
2022-04-15 11:15:17 -0600

    feat: declare variable in controller.js

---
+++ src/moudles/controller.js
@@ -0,0 +1,4 @@
+const gameId = 'Zl4d7IVkemOTTVg2fUdzz'; //
+const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+
+const gameScore = { user: '', score: 0 };
