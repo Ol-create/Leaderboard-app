a41d18bc3560c7f717de3f8d1a9fed82458f231e
Ol-create <oluola96@gmail.com>
2022-04-15 09:55:58 -0600

    feat: add main.scss file

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -17,12 +19,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/i,
+        use: [
+          'style-loader', // Inject styles into DOM
+          'css-loader', // Second-Step: Turns css into common js
+          'sass-loader', // First-Step: Turns sass into css
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
-    devServer: {
-        static: './dist',
-      },
   },
 };
