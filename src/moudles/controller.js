b180c59681d745b6e8e7f6b359ff88e1cca0163c
Ol-create <oluola96@gmail.com>
2022-04-16 05:01:36 -0600

    fix: resolve linters errs

--- src/moudles/controller.js
+++ src/moudles/controller.js
@@ -1,4 +1,4 @@
-const gameId = 'Zl4d7IVkemOTTVg2fUdzz99'; //
+const gameId = 'Zl4d7IVkemOTTVg2fUdzzoluyemipaul'; //
 const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
 
 const gameScore = { user: '', score: 0 };
